f3d535b710c4680d2a962c0dd72451a3fceae289
Jun.Hong <hong8300@gmail.com>
2022-01-06 13:56:12 +0900

    BASIC AUTH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :prevent_page_caching
+    before_action :basic_auth
 
     def prevent_page_caching
       response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    end
+
+    private
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "photron" && password == "Y2ZBKNrv" 
+      end
     end
 end
