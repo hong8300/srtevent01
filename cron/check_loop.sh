d220fb9929f7fc817b5fcdc436cd9443269d671a
Jun.Hong <hong8300@gmail.com>
2022-01-13 17:50:06 +0900

    edit shell

--- cron/check_loop.sh
+++ cron/check_loop.sh
@@ -6,33 +6,24 @@
 # cron for AWS EC2
 #
 
-LOG=/tmp/check_loop.log
+# LOG=/tmp/check_loop.log
 OS_TYPE=`uname`
-echo "---------------------------------------------" >> $LOG
-date >> $LOG
-echo "----CHECK OS TYPE----" >> $LOG
-echo $OS_TYPE >> $LOG
 
 if [ $OS_TYPE == "Darwin" ] ; then
-    echo "-----> MAC shell start" >> $LOG
+    echo "-----> MAC shell start" 
     BASE_DIR=/Users/hong/Rails/srtevent
     FFMPEG=/opt/homebrew/bin/ffmpeg
     WRITE_DIR=/Users/hong/Desktop/tmp
 elif [ $OS_TYPE == "Linux" ]; then
-    echo "-----> AWS EC2 shell start" >> $LOG
+    echo "-----> AWS EC2 shell start"
     BASE_DIR=/home/srtuser/RAILS/srtevent01
     FFMPEG=/opt/homebrew/bin/ffmpeg
     WRITE_DIR=/tmp
 else
-    echo "Unkonwn OS type" >> $LOG
+    echo "Unkonwn OS type" 
     exit 1
 fi
-
-which ruby >> $LOG
-
-RUBY=/Users/hong/.rbenv/shims/ruby
-echo $BASE_DIR >> $LOG
-
+# echo $BASE_DIR 
 ruby $BASE_DIR/cron/check_loop.rb $BASE_DIR/cron $FFMPEG $WRITE_DIR
 
 
